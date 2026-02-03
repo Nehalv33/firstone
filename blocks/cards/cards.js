c5608eefd74c9a2db9c5f7fa13a2a795a2d4371f
Nehalv33 <121609626+Nehalv33@users.noreply.github.com>
2026-02-03 16:02:44 +0530

    adding cards feature in a row

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,31 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  // Each row in your table becomes a card
+  const rows = [...block.children];
+  
+  // Loop through each row and style it as a card
+  rows.forEach((row) => {
+    row.classList.add('card-item');
+    
+    // Get the columns in each row
+    const cols = [...row.children];
+    
+    // Column 1: Title
+    if (cols[0]) cols[0].classList.add('card-title');
+    
+    // Column 2: Description
+    if (cols[1]) cols[1].classList.add('card-description');
+    
+    // Column 3: Link
+    const link = cols[2]?.querySelector('a');
+    if (link) {
+      link.classList.add('card-link');
+      link.textContent = link.textContent || 'Learn More';
+    }
+    
+    // Find image (can be in any column)
+    const image = row.querySelector('img');
+    if (image) {
+      image.parentElement.classList.add('card-image');
+    }
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.replaceChildren(ul);
 }
